5afa59a264b583457e0baad657fc7dbb2d36328d
Geniusning <690984649@qq.com>
2018-12-07 23:23:45 +0800

    测试分支

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ export default {
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    console.log('app created and cache logs by setStorageSync')
+    console.log('你是我的眼')
   }
 }
 </script>
